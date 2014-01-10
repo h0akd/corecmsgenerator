db2cb50d5339bf90cfdbed688455cb71cde29495
Mai Ngọc Hoá <h0a.kd93@gmail.com>
2014-01-10 09:24:43 +0700

    Creare package

---
+++ src/H0akd/Corecmsgenerator/CorecmsgeneratorServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace H0akd\Corecmsgenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CorecmsgeneratorServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('h0akd/corecmsgenerator');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
